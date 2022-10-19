d8bbc828035f1c3479a703c78dae394995e5f65e
Sergey Molchanov <smol4anov@yandex.ru>
2022-10-19 18:31:40 +0300

    fixed after codereview

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,41 +5,43 @@ const ERROR_CODE = 500;
 const ERROR_NOT_FOUND = 404;
 const ERROR_WRONG_DATA = 400;
 
-const handleError = (res, err) => {
-  if (err instanceof mongoose.Error.ValidationError) {
-    res.status(ERROR_WRONG_DATA).send({ message: 'Переданы некорректные данные', err });
-    return;
-  }
-  if (err.message === 'NotFound') {
-    res.status(ERROR_NOT_FOUND).send({ message: 'Запрашиваемая карточка не найдена' });
-    return;
-  }
-  if (err instanceof mongoose.Error.CastError) {
-    res.status(ERROR_WRONG_DATA).send({ message: 'Некорректный формат id', err });
-    return;
-  }
-  res.status(ERROR_CODE).send({ message: 'Произошла ошибка на сервере', err });
-};
-
 const creatCard = (req, res) => {
   const { name, link } = req.body;
   Card.create({ name, link, owner: req.user._id })
     .then((card) => card.populate('owner'))
     .then((card) => res.status(201).send(card))
-    .catch((err) => handleError(res, err));
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        res.status(ERROR_WRONG_DATA).send({ message: 'Переданы некорректные данные' });
+        return;
+      }
+      res.status(ERROR_CODE).send({ message: 'Произошла ошибка на сервере' });
+    });
 };
 
 const getCards = (req, res) => {
   Card.find({})
     .populate(['owner', 'likes'])
     .then((cards) => res.status(200).send(cards))
-    .catch((err) => handleError(res, err));
+    .catch(() => {
+      res.status(ERROR_CODE).send({ message: 'Произошла ошибка на сервере' });
+    });
 };
 
 const deleteCardById = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId).orFail(new Error('NotFound'))
     .then(() => res.status(200).send({ status: 'deleted' }))
-    .catch((err) => handleError(res, err));
+    .catch((err) => {
+      if (err.message === 'NotFound') {
+        res.status(ERROR_NOT_FOUND).send({ message: 'Запрашиваемая карточка не найдена' });
+        return;
+      }
+      if (err instanceof mongoose.Error.CastError) {
+        res.status(ERROR_WRONG_DATA).send({ message: 'Некорректный формат id' });
+        return;
+      }
+      res.status(ERROR_CODE).send({ message: 'Произошла ошибка на сервере' });
+    });
 };
 
 const setCardLike = (req, res) => {
@@ -50,7 +52,17 @@ const setCardLike = (req, res) => {
   ).orFail(new Error('NotFound'))
     .populate(['owner', 'likes'])
     .then((card) => res.status(200).send(card))
-    .catch((err) => handleError(res, err));
+    .catch((err) => {
+      if (err.message === 'NotFound') {
+        res.status(ERROR_NOT_FOUND).send({ message: 'Запрашиваемая карточка не найдена' });
+        return;
+      }
+      if (err instanceof mongoose.Error.CastError) {
+        res.status(ERROR_WRONG_DATA).send({ message: 'Некорректный формат id' });
+        return;
+      }
+      res.status(ERROR_CODE).send({ message: 'Произошла ошибка на сервере' });
+    });
 };
 
 const removeCardLike = (req, res) => {
@@ -61,7 +73,17 @@ const removeCardLike = (req, res) => {
   ).orFail(new Error('NotFound'))
     .populate(['owner', 'likes'])
     .then((card) => res.status(200).send(card))
-    .catch((err) => handleError(res, err));
+    .catch((err) => {
+      if (err.message === 'NotFound') {
+        res.status(ERROR_NOT_FOUND).send({ message: 'Запрашиваемая карточка не найдена' });
+        return;
+      }
+      if (err instanceof mongoose.Error.CastError) {
+        res.status(ERROR_WRONG_DATA).send({ message: 'Некорректный формат id' });
+        return;
+      }
+      res.status(ERROR_CODE).send({ message: 'Произошла ошибка на сервере' });
+    });
 };
 
 module.exports = {
--- controllers/users.js
+++ controllers/users.js
@@ -5,38 +5,40 @@ const ERROR_CODE = 500;
 const ERROR_NOT_FOUND = 404;
 const ERROR_WRONG_DATA = 400;
 
-const handleError = (res, err) => {
-  if (err instanceof mongoose.Error.ValidationError) {
-    res.status(ERROR_WRONG_DATA).send({ message: 'Переданы некорректные данные', err });
-    return;
-  }
-  if (err.message === 'NotFound') {
-    res.status(ERROR_NOT_FOUND).send({ message: 'Запрашиваемый пользователь не найден' });
-    return;
-  }
-  if (err instanceof mongoose.Error.CastError) {
-    res.status(ERROR_WRONG_DATA).send({ message: 'Некорректный формат id', err });
-    return;
-  }
-  res.status(ERROR_CODE).send({ message: 'Произошла ошибка на сервере', err });
-};
-
 const creatUser = (req, res) => {
   User.create(req.body)
     .then((user) => res.status(201).send(user))
-    .catch((err) => handleError(res, err));
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        res.status(ERROR_WRONG_DATA).send({ message: 'Переданы некорректные данные' });
+        return;
+      }
+      res.status(ERROR_CODE).send({ message: 'Произошла ошибка на сервере' });
+    });
 };
 
 const getUsers = (req, res) => {
   User.find({})
     .then((users) => res.status(200).send(users))
-    .catch((err) => handleError(res, err));
+    .catch(() => {
+      res.status(ERROR_CODE).send({ message: 'Произошла ошибка на сервере' });
+    });
 };
 
 const getUserById = (req, res) => {
   User.findById(req.params.userId).orFail(new Error('NotFound'))
     .then((user) => res.status(200).send(user))
-    .catch((err) => handleError(res, err));
+    .catch((err) => {
+      if (err.message === 'NotFound') {
+        res.status(ERROR_NOT_FOUND).send({ message: 'Запрашиваемый пользователь не найден' });
+        return;
+      }
+      if (err instanceof mongoose.Error.CastError) {
+        res.status(ERROR_WRONG_DATA).send({ message: 'Некорректный формат id' });
+        return;
+      }
+      res.status(ERROR_CODE).send({ message: 'Произошла ошибка на сервере' });
+    });
 };
 
 const updateUserById = (req, res) => {
@@ -50,7 +52,21 @@ const updateUserById = (req, res) => {
     },
   ).orFail(new Error('NotFound'))
     .then((user) => res.status(200).send(user))
-    .catch((err) => handleError(res, err));
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        res.status(ERROR_WRONG_DATA).send({ message: 'Переданы некорректные данные' });
+        return;
+      }
+      if (err.message === 'NotFound') {
+        res.status(ERROR_NOT_FOUND).send({ message: 'Запрашиваемый пользователь не найден' });
+        return;
+      }
+      if (err instanceof mongoose.Error.CastError) {
+        res.status(ERROR_WRONG_DATA).send({ message: 'Некорректный формат id' });
+        return;
+      }
+      res.status(ERROR_CODE).send({ message: 'Произошла ошибка на сервере' });
+    });
 };
 
 const updateUserAvatar = (req, res) => {
@@ -64,7 +80,21 @@ const updateUserAvatar = (req, res) => {
     },
   ).orFail(new Error('NotFound'))
     .then((user) => res.status(200).send(user))
-    .catch((err) => handleError(res, err));
+    .catch((err) => {
+      if (err instanceof mongoose.Error.ValidationError) {
+        res.status(ERROR_WRONG_DATA).send({ message: 'Переданы некорректные данные' });
+        return;
+      }
+      if (err.message === 'NotFound') {
+        res.status(ERROR_NOT_FOUND).send({ message: 'Запрашиваемый пользователь не найден' });
+        return;
+      }
+      if (err instanceof mongoose.Error.CastError) {
+        res.status(ERROR_WRONG_DATA).send({ message: 'Некорректный формат id' });
+        return;
+      }
+      res.status(ERROR_CODE).send({ message: 'Произошла ошибка на сервере' });
+    });
 };
 
 module.exports = {
